25f15f3379b8ba50fb0552c047f7e7b18405a666
jaypersanchez <jaypersanchez@gmail.com>
2024-03-18 06:39:13 +0800

    add readme file

---
+++ README.md
@@ -0,0 +1,11 @@
+# Requirements
+
+Aside from Visual Studio Code, you need
+
+1. FASM assebly flat compiler https://flatassembler.net/.  This will compile and link all 
+libraries.  FASM is specific to Windows only.  For Ubuntu or Linux, it's a different compiler
+altogether.
+
+For OS other than Windows you'll need NASM assembler, you can download it here https://nasm.us/
+Then you'll need a library linker like GoLink.
+
--- hello.asm
+++ hello.asm
@@ -1,8 +1,13 @@
-format PE console
-entry start
+format PE console ; opens the console for assembly to run
+entry start ; entry point of the application basically the function where app will start
 
-include 'C:\Program Files (x86)\fasmw17332\INCLUDE\win32a.inc'
+; OS specific library.  In this case Windows library. This is like import 
+; library in Javascript and Python
+include 'C:\Program Files (x86)\fasmw17332\INCLUDE\win32a.inc' 
 
+; Program code starts here up to the 'call [MessageBoxA]
+; equivalent to Javascript 'alert('Hello, World!') 
+; and Python's 'ctypes.windll.user32.MessageBoxW(0, "Hello, World!", "Hello", 0)'
 section '.text' code readable executable
 
 start:
@@ -12,14 +17,26 @@ start:
     push    0               ; hWnd = NULL
     call    [MessageBoxA]
 
+; This is to exit the application cleanly.  There's no equivalent to Javascript
+; it handles this for you.  For Python it's similar to 'sys.exit(0)
     push    0               ; Exit code
     call    [ExitProcess]
 
+; Defines data (strings) used by the program. 
+; db declares initialized data bytes. 
+; The 0 at the end of each string is a null terminator, marking the string's end.
+; Javascript/Python equivalent 
+; let message = 'Hello, World!' and message = 'Hello, World!'
 section '.data' data readable writeable
 
   message db 'Hello, World!',0
-  title   db 'Hello',0
+  title   db 'My Assembly Code',0
 
+; Assembly: Declares which DLLs are needed and which functions are imported from those DLLs. 
+; This is specific to the PE format and Windows programming.
+; JavaScript/Python Equivalent: 
+;Similar to importing functions from modules or libraries but more closely related to the 
+;operating system's functionality.
 section '.idata' import data readable writeable
 
   library kernel32,'KERNEL32.DLL',\
